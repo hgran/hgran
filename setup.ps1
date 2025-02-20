35bd6837cc0b6d21b5ae4b711716701755c2f61e
Gran <henrik.gran@if.se>
2025-02-20 13:59:11 +0100

    added setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,8 @@
+winget install --id Git.Git -e --source winget
+winget install --id Microsoft.PowerToys --source winget
+winget install --id Microsoft.PowerShell --source winget
+winget install --id JanDeDobbeleer.OhMyPosh --source winget
+winget install --id Microsoft.VisualStudioCode --source winget
+winget install --exact --id Microsoft.AzureCLI --source winget
+wsl --install
+
