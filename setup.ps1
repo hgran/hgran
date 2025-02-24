9aa1571cdf4f32e026ee279bcd6abdafdebfb5e0
Henrik Gran <henrik.gran@if.se>
2025-02-24 15:00:25 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,6 +3,8 @@ winget install --id Microsoft.PowerToys --source winget
 winget install --id Microsoft.PowerShell --source winget
 winget install --id JanDeDobbeleer.OhMyPosh --source winget
 winget install --id Microsoft.VisualStudioCode --source winget
+winget install "azure data studio"
 winget install --exact --id Microsoft.AzureCLI --source winget
+
 wsl --install
 
